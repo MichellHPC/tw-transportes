58216f8b5efa81a585de3841054ce275b485acf0
Michell_Henrick <56286200+MichellHPC@users.noreply.github.com>
2025-08-18 15:15:46 -0300

    feature: add packege from pt-br

---
+++ src/app/Filament/Resources/FreteResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FreteResource\Pages;
+use App\Filament\Resources\FreteResource\RelationManagers;
+use App\Models\Frete;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FreteResource extends Resource
+{
+    protected static ?string $model = Frete::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('origem')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('destino')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('codigo_rastreamento')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('pendente'),
+                Forms\Components\TextInput::make('remetente_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('destinatario_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('origem')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('destino')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('codigo_rastreamento')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('remetente_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('destinatario_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFretes::route('/'),
+            'create' => Pages\CreateFrete::route('/create'),
+            'view' => Pages\ViewFrete::route('/{record}'),
+            'edit' => Pages\EditFrete::route('/{record}/edit'),
+        ];
+    }
+}
